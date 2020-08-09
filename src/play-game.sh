96a7258d25f298c50daced464fdfc0234d86e7e2
Geena Smith <geena_smith@hotmail.com>
2020-08-08 17:55:14 -0700

    refactored packages. creating shell script  to run

--- src/play-game.sh
+++ src/play-game.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+# remove old files
+#(cd client && rm client) || exit 1
+#(cd server && rm server) || exit 1
+#(cd broker && rm broker) || exit 1
+
 # build
-go build client.go || exit 1
-go build server.go || exit 1
+(cd client && go build) || exit 1
+(cd server && go build) || exit 1
+(cd broker && go build) || exit 1
 
-./server &
+
+
+#./server &
 # give server time to create socket
-sleep 1
-
-./client &
-./client
-
-wait
-wait
+#sleep 1
+#
+#./client &
+#./client
+#
+#wait
+#wait
